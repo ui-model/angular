0e58a54315afa3cf326cd60d49cec2df5b26b97b
Zhicheng WANG <zcwang@thoughtworks.com>
2019-08-22 11:37:35 +0800

    fix: 支持 move-handle

--- src/lib/directives/movable-transform.directive.ts
+++ src/lib/directives/movable-transform.directive.ts
@@ -1,16 +1,18 @@
-import { Directive, HostBinding } from '@angular/core';
+import { ContentChild, Directive, HostBinding } from '@angular/core';
 import { MovableDirective } from '@ui-model/angular/src/lib/directives/movable.directive';
 
 @Directive({
   selector: '[uiMovableTransform]',
 })
 export class MovableTransformDirective {
-
-  constructor(private movable: MovableDirective) {
-  }
+  @ContentChild(MovableDirective, { static: false })
+  movable: MovableDirective;
 
   @HostBinding('style.transform')
   get transform(): string {
+    if (!this.movable) {
+      return '';
+    }
     return `translate(${this.movable.offset.x}px,${this.movable.offset.y}px)`;
   }
 }
